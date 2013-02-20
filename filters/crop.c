7e57da889566ffa7ffb0c08743bbc32edf837ce2
regukatu <morans3@rpi.edu>
2013-02-20 10:36:46 -0500

    made formatting changes; added return to finish function

---
+++ filters/crop.c
@@ -0,0 +1,11 @@
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavfilter/vf_crop.c>
+
+int crop_frame(int x, int y, int height, int width, AVCodecContext *codecContext, AVFrame *frame)
+{
+
+
+
+}
+
--- filters/resize.c
+++ filters/resize.c
@@ -2,19 +2,21 @@
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
 
-int resizeFrame(int height, int width, AVCodecContext *codecContext, AVFrame *frame) {
-	struct SwsContext resizeContext = sws_getContext (codecContext->width,
-																										codecContext->height,
-																										codecContext->pix_fmt,
-																										width,
-																										height,
-																										codecContext->pix_fmt,
-																										SWS_SPLINE,
-																										NULL,
-																										NULL,
-																										NULL );
+int resizeFrame(int height, int width, AVCodecContext *codecContext, AVFrame *frame)
+{
+	struct SwsContext resizeContext =     //creates struct with information about the input and output frames
+		sws_getContext (codecContext->width,
+							codecContext->height,
+							codecContext->pix_fmt,
+							width,
+							height,
+							codecContext->pix_fmt,
+							SWS_SPLINE,
+							NULL,
+							NULL,
+							NULL );
 		
-	sws_scale(resizeContext
+	return sws_scale(resizeContext                        //takes video stream and makes gratuitous use of magic to resize
 						(uint8_t const * const *)frame->data,
 						frame->linesize,
 						0,
