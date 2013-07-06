e9ded42e8143b45e9326911da96535e0088a39f7
DavidVorick <david.vorick@gmail.com>
2013-07-06 11:35:04 -0400

    moving files around

--- filters/frameAccurateSeek.c
+++
@@ -1,5 +0,0 @@
-// One of the goals of mkvsynth is to have an efficient way to seek to a specific frame in a file
-// There are a lot of problems doing this, but fseek (go to a specific byte in a file) and ftell (get what byte we are on) should help with this.
-// One of the big problems still remaining is that you can't read any frame you want, you have to start at an 'I' frame.
-// A complication is that there are different video codecs
-// This is not an mvp feature, therefore it isn't on the issue tracker.
