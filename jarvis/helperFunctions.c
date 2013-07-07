83fc3340f16979a9a8a2674398bdded06a77b700
DavidVorick <david.vorick@gmail.com>
2013-07-07 02:32:01 -0400

    Added documentation, changed a few of the structs around, changed getFrame and putFrame to only accept payloads and not frames themselves. One potential problem might be clearFrame, because it can't use just the payload and stop all the memory leaks, it actually needs access to the mutex and the frame itself.

--- jarvis/helperFunctions.c
+++ jarvis/helperFunctions.c
@@ -6,6 +6,46 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+///////////////////////////////////////
+// MkvsynthControlNode 
+// 	
+//	METADATA	
+// 		meant to contain information about the video stream for filters and
+// 		memory management. Currently it's only colorSpace and payloadBytes.
+// 		Technically, one should be able to determine most of the information
+// 		about the video by the resolution fields and colorspace fields, but
+// 		between sar values and rounded screens (occulus rift) it's a confusing
+// 		situation and I'm not entirely sure how best to deal with the metadata.
+// 		At some point, room for custom fields will be added.
+//
+//		height - a resolution field
+//		width - a resolution field
+//		colorDepth - number of bits used to define each color sample
+// 		colorSpace - still working out a good way to handle this
+// 		payloadBytes - needed for malloc and avoiding seg faults
+//
+//	SEMAPHORES
+//		a buffer should only have 1 filter outputting to it but may have multiple
+//		filters using it as input. Each input filter could be at a different
+//		place in the buffer, and therefore there needs to be 1 set of semaphores
+//		for each filter.
+//	
+//		outputBreadth - how many filters are using the buffer as input
+// 		reamainingBuffer - an array of semaphores indicating on a per-filter
+// 												basis how many frames are in the buffer but have
+// 												not been read by the input filter
+// 		consumedBuffer - an array of semaphores indicating on a per-filter basis
+// 												how many empty spaces are in the buffer, so that the
+// 												output filter knows if the buffer has room for more
+// 												output. The output buffer must wait until every
+// 												input filter has at least 1 empty space for a frame
+//	MOST RECENT OUTPUT FRAME
+//		each frame keeps its own pointer to the next frame, but the output filter
+//		needs to know which frame doesn't have a next frame, so that it can keep
+//		the list going.
+//
+///////////////////////////////////////
+
 typedef struct {
 	int width;
 	int height;
@@ -13,24 +53,86 @@ typedef struct {
 	int colorSpace;
 	int payloadBytes;
 
-	sem_t *remainingBuffer; // an array outputBreadth long
-	sem_t *consumedBuffer;  // an array outputBreadth long
-
 	int outputBreadth;
+	sem_t *remainingBuffer;
+	sem_t *consumedBuffer;
+
 	MkvsynthFrame *recentFrame;
 
 } MkvsynthControlNode;
+
+///////////////////////////////////////
+// MkvsynthFrame
+// 	
+//	PAYLOAD
+//		just a string of bytes, which contain information based on the meta-data.
+//		for the most part, pixels are read in left-to-right, top-to-bottom, and
+//		each pixel is fully defined before the next pixel is defined. In
+//		mkvsynth, raw frames are not compressed in any way.
+//
+//		At some point, a feature may be added that applies simple compression to
+//		the payload, using some blazing fast compression algorithm that can
+//		compress potentially repetitive data down by a fair amount. This could
+//		cause a decent sized speedup, depending on where the actual bottlenecks
+//		end up being.
+//
+//		For the time being, I'm going to establish an 'Mkvsynth Standard'
+//		colorspace, with the idea that most filters should be able to process
+//		frames within that colorspace. I'm picking RGB36, because 8 bit depth is
+//		no longer the edge of what technology can produce, and 10bit depth may
+//		not last very long. At 12bits however, we should be beyond what even
+//		trained and unusually perceptive eyes can discern. More advanced
+//		colorspaces will still availiable, but hopefully the majority of mkvsynth
+//		filters will be able to support the 'Mkvsynth Standard Colorspace'
+//
+//	FILTERS REMAINING
+//		multiple filters may be using the frame as input, before the memory can be
+//		deallocated every single filter must signal (through clearFrame) that it
+//		has finished using the frame and no longer needs to see it.
+//
+//		when input threads are reading and modifying this value, a mutex is needed
+//		to be sure that only 1 thread has access to the value at a time. Multiple
+//		filters can be accessing the payload at a time, however.
+//
+//		I'm not sure if the way I'm doing the mutexes and semaphores is the best,
+//		but I'll keep thinking about it and trying to make it faster
+//
+//	NEXT FRAME
+//		videos are just a stream of frames, and mkvsynth keeps track of the frames
+//		through a linked list. The list is maintained within the MkvsynthFrame
+//		struct.
+//
+///////////////////////////////////////
 
 typedef struct {
 	uint8_t *payload;
 
 	int filtersRemaining;
+	pthread_mutex_t lock;
 
 	MkvsynthFrame *nextFrame;
 
-	pthread_mutex_t lock;
-
 } MkvsynthFrame;
+
+///////////////////////////////////////
+// MkvsynthGetParams
+// 	
+//	FRAME DATA
+//		Each filter will be at its own space when reading the output from the
+//		previous filter, and so each filter needs some way to recognize where
+//		the current frame being viewed is.
+//
+//		payloadBytes is also kept in the controlNode, but it's repeated in this
+//		struct because an int is smaller than a pointer to the control node.
+//
+//	SEMAPHORES
+//		The real reason that getFrame doesn't just get a control node is because
+//		each filter using getFrame is looking at only 1 semaphore, that pointer
+//		is to a single semaphore and not to an array. This eliminates the need for
+//		the getFrame function to know which semaphore belongs to it instead of the
+//		other filters sharing the same control node.
+//
+///////////////////////////////////////
 
 typedef struct {
 	MkvsynthFrame *currentFrame;
@@ -41,6 +143,8 @@ typedef struct {
 
 } MkvsynthGetParams;
 
+// getFrame should probably only be returning a payload, just like putFrame
+// only takes a payload as input
 MkvsynthFrame * getFrame(MkvsynthGetParams *params) {
 	MkvsynthFrame *newFrame = malloc(sizeof(MkvsynthFrame));
 
@@ -55,6 +159,12 @@ MkvsynthFrame * getFrame(MkvsynthGetParams *params) {
 		newFrame = params->currentFrame;
 	}
 
+	// there may be some way to adjust the mutexes so that the struct does not
+	// need to be locked down while the function copies the payload from one
+	// place to another. The important part is that filtersRemaining is free
+	// from multiple things reading/modifying it at the same time - the copying
+	// is not the issue.
+
 	params->currentFrame = params->currentFrame->nextFrame;
 
 	newFrame->filtersRemaining = 0;
@@ -65,7 +175,13 @@ MkvsynthFrame * getFrame(MkvsynthGetParams *params) {
 	return newFrame;
 }
 
-void putFrame(MkvsynthControlNode *params, MkvsynthFrame *newFrame) {
+// input changed from a frame to just a payload; the filters should only
+// be worrying about the payload. Jarvis handles all of the other things
+//
+// it's late and I'm too tired to see this pointer math clearly, so I'm
+// going to update the function at a later date to reflect the fact that
+// the input was changed from a frame to just a pointer to a payload.
+void putFrame(MkvsynthControlNode *params, uint8_t *payload) {
 	int i;
 	for(i = 0; i < params->outputBreadth; i++)
 		sem_wait(params->consumedBuffer + sizeof(sem_t) * i);
