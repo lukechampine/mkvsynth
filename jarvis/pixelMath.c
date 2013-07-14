ddad2638c83ad318c6da2d21b09a8ebe88bce9bc
DavidVorick <david.vorick@gmail.com>
2013-07-14 09:17:48 -0400

    added a second set of functions to jarvis, just shells right now. They will be useful for accessing pixels within a stream of bytes, which will slow functions down but give them access to a general solution for editing videos of different colorspaces. Idea is still being polished.

---
+++ jarvis/pixelMath.c
@@ -0,0 +1,21 @@
+#ifndef _pixelMath_h_
+#define _pixelMath_h_
+
+#include "helperfunctions.c"
+
+long long getPixel(MkvsynthMetaData *metaData, uint8_t *payload, int top, int left) {
+
+}
+
+long long putPixel(MkvsynthMetaData *metaData, uint8_t *payload, int top, int left) {
+
+}
+
+long long getChannel(MkvsynthMetaData *metaData, uint8_t *payload, int top, int left, int channel) {
+
+}
+
+long long putChannel(MkvsynthMetaData *metaData, uint8_t *payload, int top, int left, int channel) {
+
+}
+#endif
