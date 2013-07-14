6a4e8118895bd1f4c2de31db99538be3929e1b46
DavidVorick <david.vorick@gmail.com>
2013-07-14 19:50:16 -0400

    small ideas

--- jarvis/pixelMath.c
+++ jarvis/pixelMath.c
@@ -1,10 +1,21 @@
 #ifndef _pixelMath_h_
 #define _pixelMath_h_
 
+#include <stdbool.h>
 #include "helperfunctions.c"
 
-long long getPixel(MkvsynthMetaData *metaData, uint8_t *payload, int top, int left) {
+bool confirmColorSpace(int colorspaceCode) {
+	if(colorspaceCode == GENERIC_RGB)
+		return true;
 
+	return false;
+}
+
+long long getPixel(MkvsynthMetaData *metaData, uint8_t *payload, int top, int left) {
+	// Need to add support/checks for RGB24,RGB32,RGB36,RGB48,RGB64
+	// For the time, other RGB values are not checked
+	//
+	// Still not quite sure how to do all of this
 }
 
 long long putPixel(MkvsynthMetaData *metaData, uint8_t *payload, int top, int left) {
