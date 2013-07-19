4b30d19c18c0f5b6607e6a05627c061bb643065e
DavidVorick <david.vorick@gmail.com>
2013-07-19 10:15:32 -0400

    takes raw frames and writes them as binary files
    I created this because I'm not sure if the 'raw data' that I'm creating is actually useful. I'm also not sure if x264 is going to be able to parse it. This filter will help with debugging.
    
    It's also a filter that I was going to make at some point anyway.

---
+++ filters/writeRawFile.c
@@ -0,0 +1,33 @@
+void writeRawFile(ASTParams *filterParams) {
+
+	///////////////////////
+	// Parameter Parsing //
+	///////////////////////
+	char *outputFileName = checkString(filterParams, "file");
+	MkvsynthInput *input = checkInput(filterParams, "input1");
+
+	////////////////////
+	// Error Checking //
+	////////////////////
+	FILE *outputFile = fopen(outputFileName, "w");
+	if(outputFile == NULL)
+		filterError("Could not open the output file!");
+
+	///////////////
+	// Meta Data //
+	///////////////
+	// Not needed on sink filters
+
+	/////////////////
+	// Filter Loop //
+	/////////////////
+	MkvsynthFrame *workingFrame = getFrame(input);
+
+	while(workingFrame != NULL) {
+		fwrite(workingFrame->payload, 1, input->metaData->bytes, outputFile);
+		clearFrame(workingFrame, NULL);
+		workingFrame = getFrame(input);
+	}
+
+	putFrame(NULL);
+}
