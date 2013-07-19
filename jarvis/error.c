2fdb0cd8e2e640ec33a6d63ee14eb61f961d7a5f
DavidVorick <david.vorick@gmail.com>
2013-07-19 16:13:25 -0400

    Update and rename error.c to startupAndTermination.c

--- jarvis/error.c
+++
@@ -1,5 +0,0 @@
-// Something should probably be added to indicate which filter failed
-void filterError(char *error) {
-	printf("A Filter Encountered An Error: %s", error);
-	exit(0);
-}
---
+++ jarvis/startupAndTermination.c
@@ -0,0 +1,18 @@
+#ifndef startupAndTermination_c_
+#define startupAndTermination_c_
+
+void signalStartupCompletion() {
+	// Empty function, just to indicate that something needs to go here	
+}
+
+// Something should probably be added to indicate which filter failed
+void filterError(char *error) {
+	printf("A Filter Encountered An Error: %s", error);
+	exit(0);
+}
+
+void terminate() {
+	pthread_exit(0);
+}
+
+#endif
