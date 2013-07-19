c63113172ac6fb204486a0e30ea429d67f5db985
DavidVorick <david.vorick@gmail.com>
2013-07-19 17:04:03 -0400

    very basic spawn() added, no waterfall starup processing yet.
    I'm not sure the best way to do the waterfall startup, because we can't just wait for the single previous input anymore. signalStarupCompletion has to wait for every single semaphore. That means we'll need a macro to go through filterParams and pick up every input thread and call a sem_wait() on it.
    
    blegh

--- jarvis/startupAndTermination.c
+++ jarvis/startupAndTermination.c
@@ -1,6 +1,11 @@
 #ifndef startupAndTermination_c_
 #define startupAndTermination_c_
 
+void spawn(ASTParams *filterParams, void *(*filter) (void *)) {
+	pthread_t newThread;
+	pthread_create(&newThread, NULL, filter, filterParams);
+}
+
 void signalStartupCompletion() {
 	// Empty function, just to indicate that something needs to go here	
 }
