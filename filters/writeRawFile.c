34e9aba74000d9494ea44db0db758952e8582cda
DavidVorick <david.vorick@gmail.com>
2013-07-26 17:30:31 -0400

    writeRawFile is now compliant with the most recent jarvis updates. Can now compile and debug.

--- filters/writeRawFile.c
+++ filters/writeRawFile.c
@@ -1,34 +1,48 @@
-void writeRawFile(ASTParams *filterParams) {
+#ifndef writeRawFile_c_
+#define writeRawFile_c_
 
-	///////////////////////
-	// Parameter Parsing //
-	///////////////////////
-	char *outputFileName = checkString(filterParams, "file");
-	MkvsynthInput *input = checkInput(filterParams, "input1");
+#include "../jarvis/datatypes.h"
 
+struct writeRawFileParams {
+	FILE *file;
+	MkvsynthInput *input;
+};
+
+MkvsynthOutput *writeRawFileDefinition(ASTnode *p, ASTnode *args) {
+	struct writeRawFileParams *params = malloc(sizeof(struct writeRawFileParams));
+	
+	checkArgs("writeRawFile", args, 2, typeStr, typeOut);
+	MkvsynthOutput *output = MANDOUT();
+	char *filename = MANDSTR();
+	
 	////////////////////
 	// Error Checking //
 	////////////////////
-	FILE *outputFile = fopen(outputFileName, "w");
+	params->file = fopen(outputFileName, "w");
 	if(outputFile == NULL)
-		filterError("Could not open the output file!");
+		filterError("Could not open the output file!");	
 
-	///////////////
-	// Meta Data //
-	///////////////
-	// Not needed on sink filters
+	params->output = createInputBuffer(output);
+	
+	mkvsynthQueue((void *)params, writeRawFile);
+	return NULL;
+}
 
+void writeRawFile(void *filterParams) {
+	struct writeRawFileParams *params = (struct writeRawFileParams *)filterParams;
+	
 	/////////////////
 	// Filter Loop //
 	/////////////////
-	MkvsynthFrame *workingFrame = getFrame(input);
+	MkvsynthFrame *workingFrame = getFrame(params->input);
 
 	while(workingFrame != NULL) {
-		fwrite(workingFrame->payload, 1, input->metaData->bytes, outputFile);
+		fwrite(workingFrame->payload, 1, params->input->metaData->bytes, params->file);
 		clearFrame(workingFrame, NULL);
 		workingFrame = getFrame(input);
 	}
 
 	putFrame(NULL);
-	mkvsynthTerminate();
 }
+
+#endif
