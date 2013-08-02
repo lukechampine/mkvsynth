0fbbc8d1de0ce3b1a26104a6f312675f228ddf56
DavidVorick <david.vorick@gmail.com>
2013-08-02 15:33:06 -0400

    Create frameControl.h

---
+++ jarvis/frameControl.h
@@ -0,0 +1,11 @@
+#ifndef frameControl_h_
+#define frameControl_h_
+
+#include <string.h>
+#include "datatypes.h"
+
+struct MkvsynthFrame *getFrame(struct MkvsynthInput *params);
+void putFrame(struct MkvsynthOutput *params, uint8_t *payload);
+void clearFrame(struct MkvsynthFrame *usedFrame);
+
+#endif
