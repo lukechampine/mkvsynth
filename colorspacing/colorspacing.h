c40d2fd3550044dd78f29a4b0a76beca9bad26e3
Sean Moran <morans3@rpi.edu>
2013-09-19 18:47:20 -0400

    formatting

--- colorspacing/colorspacing.h
+++ colorspacing/colorspacing.h
@@ -9,10 +9,10 @@
  * A pixel is a 64 bit field for data. This can be divided any way desired.    *
  *                                                                             *
  * By convention, data is always stored as the type that is closest to the     *
- * 	data size. This is for endian reason. If you are working with four fields  *
- * 	that are 2 bytes long, the type 'uint16_t' should be used. If you are      *
- *  using a single two byte field followed by a four byte field, you should    *
- *  use one uint16_t followed by a uint32_t.                                   *
+ * data size. This is for endian reason. If you are working with four fields   *
+ * that are 2 bytes long, the type 'uint16_t' should be used. If you are       *
+ * using a single two byte field followed by a four byte field, you should     *
+ * use one uint16_t followed by a uint32_t.                                    *
  ******************************************************************************/
 
 typedef struct MkvsynthPixel MkvsynthPixel;
